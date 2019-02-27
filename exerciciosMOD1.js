845e4f207f11222f499eb59c28623f796478b75d
Flavio <pedidosmr@gmail.com>
2019-02-27 12:41:20 -0300

    refactor code

--- exerciciosMOD1.js
+++ exerciciosMOD1.js
@@ -5,31 +5,24 @@ class User {
     this.email = email;
     this.password = password;
   }
-
   isAdmin () {
-    return this.admin === true;  
+    return this.admin ? "Administrator":"Default";  
   }
-
 }
-
-
 class Admin extends User {
   constructor (email, password){
     super(email, password)
     this.admin = true;
   }
-
-
 }
 
-
-
 const user1 = new User ('flavio@user.com','12345');
-const adm1 = new Admin ('adm@admn.com','2323');
+const adm1 = new Admin ('root@adm.com','#323sf23');
 
 
-console.log(user1.isAdmin());
-console.log(adm1.isAdmin());
+console.log(` Email: ${user1.email} Password: ${user1.password} user: ${user1.isAdmin()}`);
+console.log(` Email: ${adm1.email}  Password: ${adm1.password} user: ${adm1.isAdmin()}`);
+
 
 
 
